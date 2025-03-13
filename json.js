4756ae3b9caaceca59b7641d2fa0632f0904ffda
Prof Edir <ecaramba@gmail.com>
2025-03-13 20:33:01 -0300

    json

---
+++ json.js
@@ -0,0 +1,15 @@
+// JSON
+// Javascript Object Notation
+
+let caneta = {
+    cor: "Azul",
+    quant: 100,
+
+    escrever: function(){
+
+    }
+};
+
+let cidades = ["curiitba", "pinhais"];
+
+console.log(caneta);
