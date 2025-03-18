ae08ef2c3185df0681e49642ffde4f023e5ec144
Prof Edir <ecaramba@gmail.com>
2025-03-18 19:40:23 -0300

    add sqlite

---
+++ crud/index.js
@@ -0,0 +1,36 @@
+// CRUD
+// C -> Create -> Criar
+// R -> Read -> Ler
+// U -> Update -> Atualizar
+// D -> Delete 
+
+// Banco de Dados
+// SQL   -> relacional -> estruturado -> Mysql, sqlserver, oracle, postgres
+// NoSQL -> Mongodb, redis, cassandra, dinamo
+
+
+function cadastrar()
+{
+
+}
+
+function atualizar()
+{
+
+}
+
+function deletar()
+{
+
+}
+
+function pesquisar()
+{
+
+}
+
+function listar()
+{
+    
+}
+
