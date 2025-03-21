c245ec44d4279f9ca5c0fad0272c6f612268fa17
Prof Edir <ecaramba@gmail.com>
2025-03-20 21:14:31 -0300

    add form

--- crud/view/clientes.js
+++ crud/view/clientes.js
@@ -2,7 +2,41 @@ $(document).ready(function(){
 
     $.getJSON("http://localhost:3000/clientes", function(dados){
         
-        console.log(dados);
+
+        dados.forEach((item) => {
+
+            let html = '<tr>'
+                +'<td>'+ item.id +'</td>'
+                +'<td>'+ item.nome +'</td>'
+                +'<td>'+ item.email +'</td>'
+                +'<td>'+ item.telefone +'</td>'
+                +'<td>'+ item.cidade +'</td>'
+                +'<td>'+ item.idade +'</td>'
+                +'</tr>'
+            
+            $("#lista").append(html);
+
+        });
+
     }); // fim getJson
+
+    $("#bt-salvar").click(() => {
+
+        let nome = $("#nome").val();
+        let email = $("#email").val();
+
+        $("input").removeClass('is-invalid');
+
+        if (nome.trim() == '')
+        {
+            $("#nome").addClass('is-invalid');
+        }
+
+        if (email == '')
+        {
+            $("#email").addClass('is-invalid');
+        }
+
+    }); // fim do click
 
 });
